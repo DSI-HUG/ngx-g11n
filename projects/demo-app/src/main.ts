f74c8b8eba5326b0b8748d0229481ec37374bcc2
Badisi <badisi84@gmail.com>
2025-12-21 01:19:10 +0100

    feat(ng21): add support for ng21

--- projects/demo-app/src/main.ts
+++ projects/demo-app/src/main.ts
@@ -1,5 +1,5 @@
+import { provideZoneChangeDetection } from '@angular/core';
 /// <reference types="@angular/localize" />
-
 import { bootstrapApplication, platformBrowser } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
@@ -11,13 +11,13 @@ void (async (): Promise<void> => {
     // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
     if (USE_STANDALONE) {
         const { appConfig } = await import('./app/app.config');
-        bootstrapApplication(AppComponent, appConfig)
+        bootstrapApplication(AppComponent, { ...appConfig, providers: [provideZoneChangeDetection(), ...appConfig.providers] })
             .catch((err: unknown) => {
                 console.error(err);
             });
     } else {
         platformBrowser()
-            .bootstrapModule(AppModule)
+            .bootstrapModule(AppModule, { applicationProviders: [provideZoneChangeDetection()] })
             .catch((err: unknown) => {
                 console.error(err);
             });
