5eafa396ee8371b5652843df6d4fdb03bb14bdea
Badisi <badisi84@gmail.com>
2026-07-01 11:57:20 +0200

    refactor: various improvements

--- projects/lib/builders/extract-i18n/index.ts
+++ projects/lib/builders/extract-i18n/index.ts
@@ -1,10 +1,11 @@
-import { createBuilder } from '@angular-devkit/architect';
+import { type Builder, createBuilder } from '@angular-devkit/architect';
+import type { JsonObject } from '@angular-devkit/core';
 
 import { execute } from './builder';
 import type { Options } from './options';
 import type { Format } from './ori-schema';
 
-const builder = createBuilder(execute);
+const builder: Builder<Options & JsonObject> = createBuilder(execute);
 
 export type { Format, Options };
 export default builder;
